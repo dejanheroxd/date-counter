1a880dee978add4c4f8c9d7d9c6c8822271d4f5b
dejanheroxd <127946013+dejanheroxd@users.noreply.github.com>
2023-08-19 19:37:01 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="date-counter-container">
+      <DateCounter />
     </div>
   );
 }
 
-export default App;
+function DateCounter() {
+  const [inputNumber, setInputNumber] = useState(0);
+  const [sliderSteps, setSliderSteps] = useState(1);
+
+  function handleIncrement() {
+    setInputNumber(inputNumber + sliderSteps);
+  }
+
+  function handleDecrement() {
+    setInputNumber(inputNumber - sliderSteps);
+  }
+
+  function handleDirectInputChange(event) {
+    const inputValue = event.target.value;
+    isNaN(inputValue) || setInputNumber(Number(inputValue));
+  }
+
+  function handleSliderSteps(event) {
+    setSliderSteps(Number(event.target.value));
+  }
+
+  function calculateDate(days) {
+    const currentDate = new Date();
+    const calculatedDate = new Date(currentDate);
+    calculatedDate.setDate(currentDate.getDate() + days);
+    return calculatedDate.toLocaleString();
+  }
+
+  const calculatedDate = calculateDate(inputNumber);
+
+  return (
+    <>
+      <div className="slider-container">
+        <input
+          type="range"
+          min="0"
+          max="10"
+          defaultValue={1}
+          onChange={handleSliderSteps}
+        />
+        <p>Steps: {sliderSteps}</p>
+      </div>
+      <div className="input-container">
+        <button onClick={handleDecrement}>-</button>
+        <input
+          type="text"
+          value={inputNumber}
+          onChange={handleDirectInputChange}
+        />
+        <button onClick={handleIncrement}>+</button>
+      </div>
+      <p>
+        {inputNumber} days from today is {calculatedDate}
+      </p>
+    </>
+  );
+}
